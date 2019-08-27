4805b4d587eb1a30307ccbe6c30fcda87f86d8d9
Jeff Simons Decena <rjdecena@multisyscorp.com>
2019-08-27 23:16:29 +0800

    Finish poc
    Signed-off-by: Jeff Simons Decena <rjdecena@multisyscorp.com>

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -5,40 +5,74 @@
       <div class="row">
         <div class="col-sm"></div>
         <div class="col-sm-8">
-            <form>
-              <div class="form-group">
-                <label for="search" class="sr-only">Search</label>
-                <div class="input-group">
-                  <input type="text" class="form-control" placeholder="Search word meaning">
-                  <div class="input-group-append">
-                    <button type="submit" class="btn btn-primary">Search</button>
-                  </div>
+          <form v-on:submit.prevent="onSearch()">
+            <div class="form-group">
+              <label for="search" class="sr-only">Search</label>
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search word meaning" v-model="search">
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Search</button>
+                  <button type="button" v-on:click.prevent="onClear()" class="btn btn-info text-white"><i class="fa fa-eraser"></i> Clear</button>
                 </div>
               </div>
-            </form>
+            </div>
+          </form>
         </div>
         <div class="col-sm"></div>
       </div>
+      <div class="row" id="result" v-if="results.length">
+        <div class="col">
+          <div class="card" v-for="(result, idx) in results" :key="idx">
+            <div class="card-body" v-if="result.meta">
+              <h5 class="card-title">
+                <strong>{{result.meta.id}}</strong>
+                <small class="text-lowercase font-weight-light">[{{result.hwi.hw}}]</small>
+                <hr>
+              </h5>
+              <small class="text-lowercase font-weight-light"><em>{{result.fl}}</em></small>
+              <p class="card-text">{{result.shortdef[0]}}</p>
+            </div>
+          </div>
+        </div>        
+      </div>
+      <div v-if="loading" class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+  import axios from 'axios'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    data () {
+      return {
+        loading: false,
+        search: null,
+        results: []
+      }
+    },
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+      onSearch () {
+        this.results = []
+        const q = this.search
+        this.loading = true
+        axios.get('//www.dictionaryapi.com/api/v3/references/learners/json/' + q + '?key=e173e337-e26a-4e46-bb13-54b59b623001').then((res) => {
+          this.results = res.data
+          this.loading = false
+        })
+      },
+      onClear () {
+        this.results = []
+        this.search = null
       }
     }
   }
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  @import url('//fonts.googleapis.com/css?family=Source+Sans+Pro');
+  @import url('//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css');
 
   * {
     box-sizing: border-box;
@@ -48,19 +82,94 @@
 
   body { 
     font-family: 'Source Sans Pro', sans-serif;
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );    
   }
 
   #wrapper {
     height: 100vh;
     padding: 60px 80px;
     width: 100vw;
-    margin-top: 40px;
+    padding-top: 40px;
     text-align: center
   }
+
+  #wrapper #result {
+    text-align: left;
+  }
+
+  ::-webkit-input-placeholder { /* Edge */
+    color: #eee;
+  }
+
+  :-ms-input-placeholder { /* Internet Explorer 10-11 */
+    color: #eee;
+  }
+
+  ::placeholder {
+    color: #eee;
+  }
+
+  .card {
+    margin-bottom: 20px;
+  }
+
+  .card-title small {
+    color: #999;
+  }
+
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 27px;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background: rgb(0, 123, 255);
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 6px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 6px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 26px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 45px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
+  }
+
 </style>
